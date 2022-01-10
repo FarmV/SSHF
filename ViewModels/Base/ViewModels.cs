7b518160d053452746955a46aa8c672fa6770d75
Виктор FarmV <Viktor_Vovnenko@mail.ru>
2022-01-10 17:57:48 +0300

    Базовый класс модели-представления

---
+++ ViewModels/Base/ViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SSHF_.ViewModels.Base
+{
+    internal abstract class ViewModels : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+        protected virtual void OnProertyChanged([CallerMemberName] string ?PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string? PropertyName = null)
+        {
+            if(Equals(field, value)) return false;
+            field = value;
+            OnProertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
+    
