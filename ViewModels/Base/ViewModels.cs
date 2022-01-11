917a86fc244f1bc1617cae9983995c446b0ef1e6
Виктор FarmV <Viktor_Vovnenko@mail.ru>
2022-01-11 15:48:04 +0300

    Добавляем интерфейс IDisposable к классу ViewMode для примера реализации

--- ViewModels/Base/ViewModels.cs
+++ ViewModels/Base/ViewModels.cs
@@ -1,13 +1,28 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace SSHF_.ViewModels.Base
 {
-    internal abstract class ViewModels : INotifyPropertyChanged
+    internal abstract class ViewModels : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler? PropertyChanged;
 
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        private bool _Disposed;
+        protected virtual void Dispose(bool Disposing)
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+        }
 
+        //~ViewModels()
+        //{
+        //    Dispose(false);
+        //}
 
         protected virtual void OnProertyChanged([CallerMemberName] string ?PropertyName = null)
         {
