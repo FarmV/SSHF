987e66fa1ec1a27e463b7007cd93639b1c830e64
Виктор FarmV <Viktor_Vovnenko@mail.ru>
2022-01-19 06:40:58 +0300

    Чистка хлама MainWindowModel

--- Tests/SSHF+WPFTest/Models/MainWindowModel/MainWindowModel.cs
+++ Tests/SSHF+WPFTest/Models/MainWindowModel/MainWindowModel.cs
@@ -34,9 +34,6 @@ namespace SSHF_WPFTest.Models.MainWindowModel
             return lpPoint;
         }
 
-        [DllImport("user32.dll")]
-        public static extern IntPtr GetForegroundWindow();
-
         private static Point GetCursorXY()
         {
 
@@ -45,26 +42,17 @@ namespace SSHF_WPFTest.Models.MainWindowModel
             return mouse;
          
         }
+
         [DllImport("user32.dll")]
         static extern bool SetWindowPos(IntPtr handle, int handle2, int x, int y, int cx, int cy, int flag);
-
-        //[DllImport("user32.dll")]
-        //static extern IntPtr FindWindowA(string a, string b);
-
-        //static IntPtr check = FindWindowA(null, "SSHF+WPFTest");
-        
-        
-
+      
         public static Point GetWindosPosToCursor()
         {
             return App.Current.MainWindow.TranslatePoint(GetCursorXY(), App.Current.MainWindow);
         }
 
-
         private static bool CursorPosOn = false;
-
-
-      
+     
         private static void RefreshWindow()
         {
             Process a = Process.GetCurrentProcess();
@@ -77,40 +65,36 @@ namespace SSHF_WPFTest.Models.MainWindowModel
                 GetCursorPos( out point1);
                 
                 
-                    SetWindowPos(b, -1, point1.X+7, point1.Y+7, 200, 200, 0x0400);
+                   SetWindowPos(b, -1, point1.X+7, point1.Y+7, 200, 200, 0x0400);
                     //App.Current.MainWindow.Top = point.Y;
                     //App.Current.MainWindow.Left = point.X;
                 
 
             }
 
-
         }
 
         public static void ExecuteRefreshWindowOn(object? parameter)
         {
             RefreshWindow();
-            //MessageBox.Show("Привет " + Convert.ToString(parameter));
         }
 
         public static bool CanExecuteRefreshWindowOn(object? parameter)
         {
 
             return CursorPosOn is false;
-            //return TextBox1.Text != string.Empty;
         }
 
         public static void CommandExecuteRefreshWindowOFF(object? parameter)
         {
             CursorPosOn = false;
-            //MessageBox.Show("Привет " + Convert.ToString(parameter));
         }
 
         public static bool CanCommandExecuteRefreshWindowOFF(object? parameter)
         {
 
             return CursorPosOn is true;
-            //return TextBox1.Text != string.Empty;
+
         }
 
     }
