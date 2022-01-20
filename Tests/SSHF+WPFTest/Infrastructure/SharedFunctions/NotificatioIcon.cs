b0ba9098e718097eb35b19a1ab66b27e54a3a30b
Виктор FarmV <Viktor_Vovnenko@mail.ru>
2022-01-20 15:29:36 +0300

    Предварительная настройка Notification

--- Tests/SSHF+WPFTest/Infrastructure/SharedFunctions/NotificatioIcon.cs
+++ Tests/SSHF+WPFTest/Infrastructure/SharedFunctions/NotificatioIcon.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 
+using SSHF.Views.Windows.NotifyIcon;
+
 namespace SSHF.Infrastructure.SharedFunctions
 {
     internal class NotificatioIcon
@@ -25,17 +27,34 @@ namespace SSHF.Infrastructure.SharedFunctions
            // _notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon($"{AppContext.BaseDirectory}{Process.GetCurrentProcess().ProcessName}.exe");
             _notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(@"D:\Downloads\UnderRail GOG\setup_underrail_1.1.4.5_(49811).exe");
             _notifyIcon.Visible = true;
-           // _notifyIcon.Click += ClickNotifyIcon;
+            _notifyIcon.Click += ClickNotifyIcon;
+            
 
         }
 
+        Menu_icon? icon;
+        private void ClickNotifyIcon(object? sender, EventArgs e)
+        {
+            
+            if (NotificationMenuIsOpen)
+            {
+                
+                icon?.Close();
+                NotificationMenuIsOpen = false;
+                return;
+            }
+            icon = new Menu_icon();
+            NotificationMenuIsOpen = true;
+            icon.Show();
+        }
+      
 
         //public void ClickNotifyIcon(object? sender, EventArgs e)
         //{
         //    if (NotificationMenuIsOpen)
         //    {
         //        WindowCollection adssad = App.Current.Windows;
-              
+
         //        foreach (var item in App.Current.Windows)
         //        {
         //            if (item is WPF_Traslate_Test.MenuContent)
@@ -57,47 +76,46 @@ namespace SSHF.Infrastructure.SharedFunctions
         //        }
         //        return;
         //    }
-        //    NotificationMenuIsOpen = true;
-        //    App.Current.ShutdownMode = System.Windows.ShutdownMode.OnExplicitShutdown;
-        //    One.Visibility = Visibility.Collapsed;
-        //    One.Hide();
-        //    MenuContent menuContent = new MenuContent(this);
-        //    Point positionCursor = GetCursorPosition();
-        //    menuContent.Topmost = true;
-        //    double resolutionWidth = SystemParameters.PrimaryScreenWidth;
-        //    double resolutionHeight = SystemParameters.PrimaryScreenHeight;
+            //NotificationMenuIsOpen = true;
+            //App.Current.ShutdownMode = System.Windows.ShutdownMode.OnExplicitShutdown;
+            //One.Visibility = Visibility.Collapsed;
+            //One.Hide();
+            //MenuContent menuContent = new MenuContent(this);
+            //Point positionCursor = GetCursorPosition();
+            //menuContent.Topmost = true;
+            //double resolutionWidth = SystemParameters.PrimaryScreenWidth;
+            //double resolutionHeight = SystemParameters.PrimaryScreenHeight;
 
-        //    WindowCollection windowsMyApp = App.Current.Windows;
+            //WindowCollection windowsMyApp = App.Current.Windows;
 
-        //    double posT = menuContent.Top = positionCursor.Y - 200.00;
-        //    double posL = menuContent.Left = positionCursor.X + 5;
-        //    menuContent.Show();
-        //    double menuWidth = default;
-        //    double menuHeight = default;
-        //    foreach (var item in App.Current.Windows)
-        //    {
-        //        if (item is WPF_Traslate_Test.MenuContent)
-        //        {
-        //            MenuContent menu = (MenuContent)item;
+            //double posT = menuContent.Top = positionCursor.Y - 200.00;
+            //double posL = menuContent.Left = positionCursor.X + 5;
+            //menuContent.Show();
+            //double menuWidth = default;
+            //double menuHeight = default;
+            //foreach (var item in App.Current.Windows)
+            //{
+            //    if (item is WPF_Traslate_Test.MenuContent)
+            //    {
+            //        MenuContent menu = (MenuContent)item;
 
-        //            menuWidth = menu.ActualWidth;
-        //            menuHeight = menu.ActualHeight;
-        //        }
-        //    }
+            //        menuWidth = menu.ActualWidth;
+            //        menuHeight = menu.ActualHeight;
+            //    }
+            //}
 
-        //    if (menuWidth + positionCursor.X > resolutionWidth)
-        //    {
-        //        menuContent.Left = resolutionWidth - menuWidth;
-        //        menuContent.Left = positionCursor.X - (menuWidth + 5);
-        //    }
-        //    if (menuHeight + positionCursor.Y < resolutionHeight)
-        //    {
-        //        menuContent.Top = resolutionHeight - menuHeight;
-        //        menuContent.Top = positionCursor.Y - (menuHeight - 200.00);
-        //    }
+            //if (menuWidth + positionCursor.X > resolutionWidth)
+            //{
+            //    menuContent.Left = resolutionWidth - menuWidth;
+            //    menuContent.Left = positionCursor.X - (menuWidth + 5);
+            //}
+            //if (menuHeight + positionCursor.Y < resolutionHeight)
+            //{
+            //    menuContent.Top = resolutionHeight - menuHeight;
+            //    menuContent.Top = positionCursor.Y - (menuHeight - 200.00);
+            //}
 
-
-        //}
+            // }
 
         public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject /// невозможно перебрать элементы если окно не отображется
         {
