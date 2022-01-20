bcc75c5348cb5324095755840fe15ee54738ad14
Виктор FarmV <Viktor_Vovnenko@mail.ru>
2022-01-20 13:12:01 +0300

    Начальная интеграция Функции запуска одной копии

---
+++ Tests/SSHF+WPFTest/Infrastructure/SharedFunctions/SingleProgram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SSHF.Infrastructure.SharedFunctions
+{
+    internal class SingleProgram
+    {
+
+
+        private static volatile Mutex? InstanceCheckMutex;
+
+
+        private static bool SingleProgramCheck()
+        {
+            bool isNew = true;
+            InstanceCheckMutex = new Mutex(true, "MyMutexSingleProgramCheck", out isNew);
+            return isNew;
+
+        }
+        void OFFthisAPP()
+        {
+            throw new NotImplementedException();
+            if (!SingleProgramCheck())
+            {
+                App.Current.Shutdown();
+               // this.Dispose();
+                return;
+            }
+        }
+    }
+}
