3e69711a539d8d986a751cb1e44bd87a76f4a858
Виктор FarmV <Viktor_Vovnenko@mail.ru>
2022-01-21 06:38:42 +0300

    Изменение логики клика нотификации

--- Tests/SSHF+WPFTest/Infrastructure/SharedFunctions/NotificatioIcon.cs
+++ Tests/SSHF+WPFTest/Infrastructure/SharedFunctions/NotificatioIcon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -24,21 +25,42 @@ namespace SSHF.Infrastructure.SharedFunctions
 
         private void SetIconToMainApplication()
         {
-           // _notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon($"{AppContext.BaseDirectory}{Process.GetCurrentProcess().ProcessName}.exe");
+            // _notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon($"{AppContext.BaseDirectory}{Process.GetCurrentProcess().ProcessName}.exe");
             _notifyIcon.Icon = System.Drawing.Icon.ExtractAssociatedIcon(@"D:\Downloads\UnderRail GOG\setup_underrail_1.1.4.5_(49811).exe");
             _notifyIcon.Visible = true;
-            _notifyIcon.Click += ClickNotifyIcon;
-            
+            // _notifyIcon.Click += ClickNotifyIcon;
 
+            _notifyIcon.MouseDown += _notifyIcon_MouseDown;
+        }
+
+        private void _notifyIcon_MouseDown(object? sender, System.Windows.Forms.MouseEventArgs e)
+        {
+
+            System.Windows.Forms.MouseButtons buttonMouse = e.Button;
+
+
+
+            if (NotificationMenuIsOpen && buttonMouse == System.Windows.Forms.MouseButtons.Left)
+            {
+                icon?.Close();
+                NotificationMenuIsOpen = false;
+                return;
+            }
+            if (!NotificationMenuIsOpen && buttonMouse == System.Windows.Forms.MouseButtons.Left)
+            {
+                icon = new Menu_icon();
+                NotificationMenuIsOpen = true;
+
+            }
         }
 
         Menu_icon? icon;
         private void ClickNotifyIcon(object? sender, EventArgs e)
         {
-            
+
             if (NotificationMenuIsOpen)
             {
-                
+
                 icon?.Close();
                 NotificationMenuIsOpen = false;
                 return;
@@ -47,7 +69,7 @@ namespace SSHF.Infrastructure.SharedFunctions
             NotificationMenuIsOpen = true;
             icon.Show();
         }
-      
+
 
         //public void ClickNotifyIcon(object? sender, EventArgs e)
         //{
@@ -76,46 +98,46 @@ namespace SSHF.Infrastructure.SharedFunctions
         //        }
         //        return;
         //    }
-            //NotificationMenuIsOpen = true;
-            //App.Current.ShutdownMode = System.Windows.ShutdownMode.OnExplicitShutdown;
-            //One.Visibility = Visibility.Collapsed;
-            //One.Hide();
-            //MenuContent menuContent = new MenuContent(this);
-            //Point positionCursor = GetCursorPosition();
-            //menuContent.Topmost = true;
-            //double resolutionWidth = SystemParameters.PrimaryScreenWidth;
-            //double resolutionHeight = SystemParameters.PrimaryScreenHeight;
+        //NotificationMenuIsOpen = true;
+        //App.Current.ShutdownMode = System.Windows.ShutdownMode.OnExplicitShutdown;
+        //One.Visibility = Visibility.Collapsed;
+        //One.Hide();
+        //MenuContent menuContent = new MenuContent(this);
+        //Point positionCursor = GetCursorPosition();
+        //menuContent.Topmost = true;
+        //double resolutionWidth = SystemParameters.PrimaryScreenWidth;
+        //double resolutionHeight = SystemParameters.PrimaryScreenHeight;
 
-            //WindowCollection windowsMyApp = App.Current.Windows;
+        //WindowCollection windowsMyApp = App.Current.Windows;
 
-            //double posT = menuContent.Top = positionCursor.Y - 200.00;
-            //double posL = menuContent.Left = positionCursor.X + 5;
-            //menuContent.Show();
-            //double menuWidth = default;
-            //double menuHeight = default;
-            //foreach (var item in App.Current.Windows)
-            //{
-            //    if (item is WPF_Traslate_Test.MenuContent)
-            //    {
-            //        MenuContent menu = (MenuContent)item;
+        //double posT = menuContent.Top = positionCursor.Y - 200.00;
+        //double posL = menuContent.Left = positionCursor.X + 5;
+        //menuContent.Show();
+        //double menuWidth = default;
+        //double menuHeight = default;
+        //foreach (var item in App.Current.Windows)
+        //{
+        //    if (item is WPF_Traslate_Test.MenuContent)
+        //    {
+        //        MenuContent menu = (MenuContent)item;
 
-            //        menuWidth = menu.ActualWidth;
-            //        menuHeight = menu.ActualHeight;
-            //    }
-            //}
+        //        menuWidth = menu.ActualWidth;
+        //        menuHeight = menu.ActualHeight;
+        //    }
+        //}
 
-            //if (menuWidth + positionCursor.X > resolutionWidth)
-            //{
-            //    menuContent.Left = resolutionWidth - menuWidth;
-            //    menuContent.Left = positionCursor.X - (menuWidth + 5);
-            //}
-            //if (menuHeight + positionCursor.Y < resolutionHeight)
-            //{
-            //    menuContent.Top = resolutionHeight - menuHeight;
-            //    menuContent.Top = positionCursor.Y - (menuHeight - 200.00);
-            //}
+        //if (menuWidth + positionCursor.X > resolutionWidth)
+        //{
+        //    menuContent.Left = resolutionWidth - menuWidth;
+        //    menuContent.Left = positionCursor.X - (menuWidth + 5);
+        //}
+        //if (menuHeight + positionCursor.Y < resolutionHeight)
+        //{
+        //    menuContent.Top = resolutionHeight - menuHeight;
+        //    menuContent.Top = positionCursor.Y - (menuHeight - 200.00);
+        //}
 
-            // }
+        // }
 
         public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject /// невозможно перебрать элементы если окно не отображется
         {
