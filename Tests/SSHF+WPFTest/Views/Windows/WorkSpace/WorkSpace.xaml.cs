0c7e351f6f2ee5a7b4d3414a4246aa0208b94abb
Виктор FarmV <Viktor_Vovnenko@mail.ru>
2022-02-21 03:29:53 +0300

    test -- tags dev

---
+++ Tests/SSHF+WPFTest/Views/Windows/WorkSpace/WorkSpace.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SSHF.Views.Windows.WorkSpace
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkSpace.xaml
+    /// </summary>
+    public partial class WorkSpace: Window
+    {
+        public WorkSpace()
+        {
+            InitializeComponent();
+        }
+    }
+}
