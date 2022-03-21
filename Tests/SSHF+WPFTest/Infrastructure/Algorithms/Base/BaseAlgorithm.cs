3d7bd5e365db2d59f4bae91afd44da4910ed4b5f
Виктор FarmV <Viktor_Vovnenko@mail.ru>
2022-03-21 12:10:49 +0300

    Правка

--- Tests/SSHF+WPFTest/Infrastructure/Algorithms/Base/BaseAlgorithm.cs
+++ Tests/SSHF+WPFTest/Infrastructure/Algorithms/Base/BaseAlgorithm.cs
@@ -57,25 +57,36 @@ namespace SSHF.Infrastructure.Algorithms.Base
     internal static class ExHelp
     {
         internal static Lazy<KeyValuePair<T1, T2>[]> GetLazzyDictionaryFails<T1, T2>(params KeyValuePair<T1, T2>[] dictionary) where T1 : Enum
-            => new Lazy<KeyValuePair<T1, T2>[]>(new Func<KeyValuePair<T1, T2>[]>(() => dictionary));
+            => new Lazy<KeyValuePair<T1, T2>[]>(() => dictionary);
         internal static Exception Report<T,T2>(this Exception ex, KeyValuePair<T, T2> report) where T: Enum
         {
             ex.Data[report.Key] = report.Value;
             return ex;
         }
-
-
+        
         internal enum HelpOperation
         {
             None,
             Canecled,
             InvalidDirectory
         }
-      
+
+
+        enum HelerReasonFailFail
+        {
+            None,
+            InconsistencyEnum
+        }
         internal static string HelerReasonFail(HelpOperation operation, Type? type = null)
         {
+            if (Enum.IsDefined(typeof(HelpOperation), operation)) throw new InvalidCastException().
+                    Report(new KeyValuePair<HelerReasonFailFail,string>(HelerReasonFailFail.InconsistencyEnum,$"В {nameof(HelerReasonFailFail)} не присутвует ключ"));
+
             if(operation is HelpOperation.Canecled) return $"Операция полчуила {typeof(CancellationToken)} и была оменена";
             if (operation is HelpOperation.InvalidDirectory) return $"Неверная директория {nameof(type)}";
+
+
+            throw new Exception($"Непредвиденная ошибка {nameof(HelerReasonFail)}");
         }
     }
 }
