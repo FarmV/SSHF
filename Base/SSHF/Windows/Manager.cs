a4328fd84f8815c424197b68fe81012e1c29cf87
Viсtor Vovnenko <Viktor-FarmV@outlook.com>
2023-06-02 21:21:47 +0300

    Изменение версии

---
+++ Base/SSHF/Windows/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SSHF.Windows
+{
+    internal class Manager
+    {
+        private Dictionary<string, Window> _keyValuePairs = new Dictionary<string, Window>();
+        public Manager()
+        {
+        }
+
+       private void SetWindows(Window window)
+       {
+            _keyValuePairs[window.Title] = window;          
+       }
+        private Window GetWindow(string key)
+        {
+            return _keyValuePairs[key];
+        }
+    }
+}
