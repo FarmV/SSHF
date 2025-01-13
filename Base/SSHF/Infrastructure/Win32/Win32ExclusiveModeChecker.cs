a261542e04defc2a85325b789434ae1c51dd6cc2
Viсtor Vovnenko <Viktor-FarmV@outlook.com>
2025-01-13 18:20:05 +0300

    Правка нейминга

--- Base/SSHF/Infrastructure/Win32/Win32ExclusiveModeChecker.cs
+++ Base/SSHF/Infrastructure/Win32/Win32ExclusiveModeChecker.cs
@@ -12,8 +12,8 @@ namespace FVH.SSHF.Infrastructure.Win32
     {
         private static Guid CLSID_DirectDraw7 = new Guid("3C305196-50DB-11D3-9CFE-00C04FD930C5");
         private static Guid IID_IDirectDraw7 = new Guid("15E65EC0-3B9C-11D2-B92F-00609797EA5B");
-        private const int _DD_OK = 0;
-        private const int _DDERR_EXCLUSIVEMODEALREADYSET = unchecked((int)0x88760245);
+        private const int DD_OK = 0;
+        private const int DDERR_EXCLUSIVEMODEALREADYSET = unchecked((int)0x88760245);
         private bool _isDispose = false;
         private readonly IDirectDraw7 _idd7;
         internal Win32ExclusiveModeChecker()
@@ -22,7 +22,7 @@ namespace FVH.SSHF.Infrastructure.Win32
             if(Activator.CreateInstance(directDraw7) is not IDirectDraw7 iDirectDraw7) throw new ArgumentNullException(nameof(iDirectDraw7));
             Guid emptyInitialize = Guid.Empty;
             HRESULT resultInitialize = iDirectDraw7.Initialize(ref emptyInitialize);
-            if(resultInitialize != _DD_OK) Marshal.ThrowExceptionForHR(resultInitialize);
+            if(resultInitialize != DD_OK) Marshal.ThrowExceptionForHR(resultInitialize);
 
             _idd7 = iDirectDraw7;
         }
@@ -34,7 +34,7 @@ namespace FVH.SSHF.Infrastructure.Win32
             GC.SuppressFinalize(this);
         }
         ~Win32ExclusiveModeChecker() { if(_isDispose is true) return; Dispose(); }
-        internal bool CheckExclusiveMode(Dispatcher staDispatcher) => staDispatcher.Invoke(() => _idd7.TestCooperativeLevel() == _DDERR_EXCLUSIVEMODEALREADYSET);
+        internal bool CheckExclusiveMode(Dispatcher staDispatcher) => staDispatcher.Invoke(() => _idd7.TestCooperativeLevel() == DDERR_EXCLUSIVEMODEALREADYSET);
     }
 }
 
