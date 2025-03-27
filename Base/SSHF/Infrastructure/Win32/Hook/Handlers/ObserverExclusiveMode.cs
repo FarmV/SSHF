6731528fffede5596fe03622957965b6b692e355
Viсtor Vovnenko <Viktor-FarmV@outlook.com>
2025-03-27 14:27:45 +0300

    Структура и стилистика

---
+++ Base/SSHF/Infrastructure/Win32/Hook/Handlers/ObserverExclusiveMode.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Threading;
+
+using R3;
+
+
+namespace FVH.SSHF.Infrastructure.Win32
+{
+    internal partial class ObserverExclusiveMode : IDisposable
+    {
+        private bool _isDispose = false;
+        private bool _isExcusiveMode = false;
+        internal Win32ExclusiveModeChecker _exclusiveModeChecker;
+        internal readonly R3.BehaviorSubject<bool> ExcusiveMode;
+        private readonly Dispatcher _dispatcher;
+        internal ObserverExclusiveMode(Dispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+            _exclusiveModeChecker = _dispatcher.Invoke(()=> _ = new Win32ExclusiveModeChecker());
+
+            ExcusiveMode = new R3.BehaviorSubject<bool>(false);
+
+            ArgumentNullException.ThrowIfNull(_exclusiveModeChecker);
+        }
+        public void Dispose()
+        {
+            if(_isDispose) return;
+            _isDispose = true;
+            ExcusiveMode.OnCompleted(Result.Success);
+            ExcusiveMode.Dispose();
+            _dispatcher.Invoke(() => _exclusiveModeChecker.Dispose());
+        }
+        internal void CheckAndSetStateExcusiveMode()
+        {
+            bool isExcusiveMode = false;
+            if(ExcusiveMode.Value == false)
+            {
+                TimeSpan empiricalTimeoutSpinWait = TimeSpan.FromMilliseconds(25); // Предполагаемая задержка между получение фокуса окна и установкой режима
+                _ = SpinWait.SpinUntil(() =>
+                {
+                    isExcusiveMode = _exclusiveModeChecker.CheckExclusiveMode(_dispatcher);
+                    return isExcusiveMode is true;
+                }, empiricalTimeoutSpinWait);
+            }
+            else { isExcusiveMode = _exclusiveModeChecker.CheckExclusiveMode(_dispatcher); }
+
+            _isExcusiveMode = isExcusiveMode;
+            if(_isExcusiveMode is true) { if(Thread.CurrentThread.InUIThreadTimeCriticalSection() is false) _ = Thread.CurrentThread.StartUITimeCriticalSectionThrowIfNotUIThread(); }
+            ExcusiveMode.OnNext(_isExcusiveMode);
+        }                               
+    }
+}
---
+++ Base/SSHF/Infrastructure/Win32/Hook/Handlers/ObserverMsScreenClipExecuting.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+using R3;
+
+using Windows.Win32.Foundation;
+
+
+namespace FVH.SSHF.Infrastructure.Win32
+{
+    internal partial class ObserverMsScreenClipExecuting
+    {
+        private const string MsScreenClipPath = "C:\\Windows\\SystemApps\\MicrosoftWindows.Client.CBS_cw5n1h2txyewy\\ScreenClippingHost.exe";
+        private readonly HashSet<Process> _msScreenClipExecutingSet;
+        internal readonly R3.BehaviorSubject<bool> IsExecutingProcessScreenClip;
+        public ObserverMsScreenClipExecuting()
+        {
+            _msScreenClipExecutingSet = new HashSet<Process>();
+            IsExecutingProcessScreenClip = new BehaviorSubject<bool>(false);
+        }
+        internal void CheckAndSetStateMsScreenClipExecuting(ref nint handleWindow)
+        {
+            void ProcessExitedEvent(object? proc, EventArgs _)
+            {
+                if(proc is not Process pr) throw new InvalidCastException();
+                pr.Exited -= ProcessExitedEvent;
+                pr.Dispose();
+                IsExecutingProcessScreenClip.OnNext(false);
+            }
+            _ = GetWindowThreadProcessId(new HWND(handleWindow), out uint procID);
+            Process pr = System.Diagnostics.Process.GetProcessById((int)procID);
+            if(pr.MainModule is null)
+            {
+                pr.Dispose();
+                return;
+            }
+            if(pr.MainModule.FileName == MsScreenClipPath)
+            {
+                if(pr.HasExited is true) return;
+                if(_msScreenClipExecutingSet.Contains(pr) is true) return;
+                _ = _msScreenClipExecutingSet.Add(pr);
+                pr.EnableRaisingEvents = true;
+
+                pr.Exited += ProcessExitedEvent;
+
+                IsExecutingProcessScreenClip.OnNext(true);
+            }
+        }              
+        [DllImport("user32")]
+        private static extern uint GetWindowThreadProcessId(HWND hWnd, out uint lpdwProcessId);
+    }
+}
