d18308c75cbf50d65af349fcbf40522b16066cbd
Viktor Vovnenko | FarmV <Viktor-FarmV@outlook.com>
2025-09-04 22:25:12 +0300

    Обвнить зависмости повысит до 0.8.12

--- Base/SSHF/Windows/FastWindow/Manager/FastWindowViewModelDependencies.cs
+++ Base/SSHF/Windows/FastWindow/Manager/FastWindowViewModelDependencies.cs
@@ -8,16 +8,16 @@ namespace FVH.SSHF.FastWindowArea
     internal class FastWindowViewModelDependencies : IDisposable
     {
         internal bool IsDisposed = false;
-        private IGetImage _iGetImage;
+        private ImageProvider _iGetImage;
         private IWindowPositionUpdater? _iWindowPositionUpdater;
         private WPFDpiCorrector? _dpiCorrector;
         private WPFDropImageFile? _setImage;
 
-        internal FastWindowViewModelDependencies(IGetImage imageProvider)
+        internal FastWindowViewModelDependencies(ImageProvider imageProvider)
         {
             _iGetImage = imageProvider;
         }
-        internal IGetImage IGetImage { get { ObjectDisposedException.ThrowIf(IsDisposed, this); return _iGetImage; } init => _iGetImage = value; }
+        internal ImageProvider IGetImage { get { ObjectDisposedException.ThrowIf(IsDisposed, this); return _iGetImage; } init => _iGetImage = value; }
         internal IWindowPositionUpdater? IWindowPositionUpdater { get { ObjectDisposedException.ThrowIf(IsDisposed, this); return _iWindowPositionUpdater; } set => _iWindowPositionUpdater = value; }
         internal WPFDpiCorrector? DpiCorrector { get { ObjectDisposedException.ThrowIf(IsDisposed, this); return _dpiCorrector; } set => _dpiCorrector = value; }
         internal WPFDropImageFile? SetImage { get { ObjectDisposedException.ThrowIf(IsDisposed, this); return _setImage; } set => _setImage = value; }
